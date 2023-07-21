8dc499984df9681d4d716738c33eaa6a401094d4
liliu do carmo <32183622+liliul@users.noreply.github.com>
2023-07-21 18:46:29 -0300

    feat: housing-location

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     CommonModule,
+    HousingLocationComponent
   ],
   template: `
     <section>
@@ -13,6 +16,9 @@ import { CommonModule } from '@angular/common';
         <input type="text" placeholder="Filter by city">
         <button class="primary" type="button">Search</button>
       </form>
+    </section>
+    <section class="results">
+      <app-housing-location></app-housing-location>
     </section>
   `,
   styleUrls: ['./home.component.css'],
