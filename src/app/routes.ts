d69864659fb073efe7b128c1e38b140bcbf789a2
liliul <liliuestudo@gmail.com>
2023-07-26 00:13:12 -0300

    fix: router componenteCasa

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+import { componenteCasa } from './home/home.component';
 import { DetalhesDoComponente } from './details/details.component';
 
 const routeConfig: Routes = [
     {
       path: '',
-      component: HomeComponent,
+      component: componenteCasa,
       title: 'Home page'
     },
     {
